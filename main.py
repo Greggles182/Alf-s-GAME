a8ef2569c0cf1300ea098a4a5ec5d5378424ac99
Gregory Hammond <76397590+Greggles182@users.noreply.github.com>
2022-05-31 13:34:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+Myspecialtext = "THIS THEXT IS AMAZING!!"
+mynumber = 2012
+print("Hello World!")
+print("gregory!")
+print(Myspecialtext)
+print(2011)
+print(mynumber)
+print("Alfred!")
+print("""
+			Alfred is bad at coding,\n
+			Gregory is good,\n
+			BUT EVERYTHING GOES ON UNDER THE HOOD.\n
+			""")
